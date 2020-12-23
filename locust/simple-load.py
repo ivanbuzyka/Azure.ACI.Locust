e31c8b0d44f486b66b8c73c03b217e9ee7178b86
ivanbuzyka <ivanbuzyka@gmail.com>
2020-12-23 16:42:04 +0100

    added examples and quides to the Python script

--- locust/simple-load.py
+++ locust/simple-load.py
@@ -8,7 +8,14 @@ class QuickstartUser(HttpUser):
     def index_page(self):
         self.client.get("/")
 
-    @task(3)
+        # Add parameter verify=False if you are using self-signed/invalid certificate 
+        # self.client.get("/", verify=False)
+
+        # You can also use hardcoded URL which may be important in scenarios when multisite app is load tested.
+        # Second name parametert is important to collect statistic correctly
+        # self.client.get("https://sitehostname/", name="https://sitehostname/", verify=False)
+
+    @task
     def view_item(self):
         item_id = random.randint(1, 10000)
         self.client.get(f"/?id={item_id}", name="/?id=somerandomstring")
